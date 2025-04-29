f778b238dbc4d3b1f465b93914b9f653164f3cce
Andrew Richard <acrichard07@gmail.com>
2025-04-28 19:14:03 -0700

    new weights

--- cat_weights_streamlit.py
+++ cat_weights_streamlit.py
@@ -1,21 +1,22 @@
 """
-Cat weights app created in streamlit 
+Cat weights app created in streamlit
 Andrew Richard
 2025-02-19
 """
 
-import streamlit as st
-import pandas as pd
-import numpy as np
 from datetime import datetime as dt
-import time
+
+import pandas as pd
+import streamlit as st
+
 from funcs import age_in_weeks, haruki_age, sullivan_age
 
 # App title
 st.title("change in kitten weights over time")
 
 # path to dataset
-path = 'weights.csv'
+path = "weights.csv"
+
 
 @st.cache_data
 def load_weights(path):
@@ -23,31 +24,54 @@ def load_weights(path):
     weights = age_in_weeks(weights)
     return weights
 
+
 # load weights in as weight
 weights = load_weights(path)
 # add checkbox to optionally toggle raw data display
-if st.checkbox('show raw data'):
-    st.subheader('raw data')
+if st.checkbox("show raw data"):
+    st.subheader("raw data")
     st.write(weights)
 
 
 # create line chart for cat weights by age in weeks
 st.subheader("weight change by age in weeks")
-st.line_chart(weights, x='weeks', y='weight', color='cat', x_label="age (wks)", y_label="weight (lbs)")
+st.line_chart(
+    weights,
+    x="weeks",
+    y="weight",
+    color="cat",
+    x_label="age (wks)",
+    y_label="weight (lbs)",
+)
 
 # create line chart for cat weights by date
 st.subheader("weight change by date")
-st.line_chart(weights, x='date', y='weight', color='cat', x_label="date", y_label="weight (lbs)")
+st.line_chart(
+    weights,
+    x="date",
+    y="weight",
+    color="cat",
+    x_label="date",
+    y_label="weight (lbs)",
+)
 
+# st.altair_chart(weights).mark_line().encode(
+#     x=alt.X(
+#         "date",
+#     )
+# )
 
 # add cat ages
-today_date_str = dt.today().strftime('%Y-%m-%d')
+today_date_str = dt.today().strftime("%Y-%m-%d")
 sully_age = sullivan_age(today_date_str)
 ruki_age = haruki_age(today_date_str)
 
-# sullivan age 
-st.write(f"Sullivan's age: {sully_age['years']} years, {sully_age['months']} months, {sully_age['weeks']} weeks, {sully_age['days']} days")
+# sullivan age
+st.write(
+    f"Sullivan's age: {sully_age['years']} years, {sully_age['months']} months, {sully_age['weeks']} weeks, {sully_age['days']} days"
+)
 
-# haruki age 
-st.write(f"Haruki's age: {ruki_age['years']} years, {ruki_age['months']} months, {ruki_age['weeks']} weeks, {ruki_age['days']} days")
-
+# haruki age
+st.write(
+    f"Haruki's age: {ruki_age['years']} years, {ruki_age['months']} months, {ruki_age['weeks']} weeks, {ruki_age['days']} days"
+)
